1420f38f94ab08be5c44ce7b8e7b808d2c5f8c45
Stephen Meckstroth <meckstss@gmail.com>
2021-02-14 20:11:20 -0500

    Setup SpringBoot

---
+++ src/main/java/com/hobbycollection/HobbyCollectionApplication.java
@@ -0,0 +1,13 @@
+package com.hobbycollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HobbyCollectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HobbyCollectionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hobbycollection/HobbyCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hobbycollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HobbyCollectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
