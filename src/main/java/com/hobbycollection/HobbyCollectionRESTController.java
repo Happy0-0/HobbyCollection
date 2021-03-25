b0e4d13b03f26506b4b9f19f224598c2fb9653bb
Benjamin Romig <romigbl@mail.uc.edu>
2021-03-25 18:39:29 -0400

    Changed reference names for css classes and added documentation

--- src/main/java/com/hobbycollection/HobbyCollectionRESTController.java
+++ src/main/java/com/hobbycollection/HobbyCollectionRESTController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HobbyCollectionRESTController {
 
     /**
-     *
+     * Fetches a collection by the id
      * @param id
      * @return URL
      */
