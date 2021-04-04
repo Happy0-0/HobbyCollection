fcbabcf1ab3aca289e7d3e35c3f5dec003c953b3
smitty891 <smitty891@users.noreply.github.com>
2021-04-04 19:40:19 -0400

    Adding the @EnableCaching annotation to the application class to enable caching.

--- src/main/java/com/hobbycollection/HobbyCollectionApplication.java
+++ src/main/java/com/hobbycollection/HobbyCollectionApplication.java
@@ -2,8 +2,10 @@ package com.hobbycollection;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class HobbyCollectionApplication {
 
     public static void main(String[] args) {
